d28fb630d77d5c1f25a7659e49aedd8b991feb67
Sheigutn <fb020198@gmail.com>
2015-09-28 18:40:51 +0200

    Forgot to add a javadoc comment

--- src/main/java/com/github/sheigutn/pushbullet/interfaces/Blockable.java
+++ src/main/java/com/github/sheigutn/pushbullet/interfaces/Blockable.java
@@ -3,5 +3,8 @@ package com.github.sheigutn.pushbullet.interfaces;
 @FunctionalInterface
 public interface Blockable {
 
+    /**
+     * Used to block this object (most likely a user)
+     */
     void block();
 }
